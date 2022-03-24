9c8f8ea94bc35b669be62c9f3e3b8e39c06b2fa8
Siddhesh Kothadi <55179845+siddheshkothadi@users.noreply.github.com>
2022-03-24 11:31:30 +0000

    Adds model prediction code

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const cors = require("cors");
 const express = require("express");
 const multer = require("multer");
+const fs = require('fs');
+const Jimp = require('jimp');
+
+const tf = require('@tensorflow/tfjs-node');
+
+tf.loadLayersModel('file://model/model.json').then((model) => {
+  global.model = model;
+});
 
 const app = express();
 app.use(cors());
@@ -23,14 +31,60 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage});
 
+async function getPredictions(imagePath) {
+  if(model === undefined) {
+    setTimeout(() => {
+      getPredictions(imagePath);
+    }, 100);
+  }
+  else {
+    try {
+      const image = await Jimp.read(`/home/runner/file-upload-server/${imagePath}`);
+    
+      image.cover(224, 224, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
+    
+      const NUM_OF_CHANNELS = 3;
+    
+      let values = new Float32Array(224 * 224 * NUM_OF_CHANNELS);
+    
+      let i = 0;
+      image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+        const pixel = Jimp.intToRGBA(image.getPixelColor(x, y));
+        pixel.r = pixel.r / 127.0 - 1;
+        pixel.g = pixel.g / 127.0 - 1;
+        pixel.b = pixel.b / 127.0 - 1;
+        pixel.a = pixel.a / 127.0 - 1;
+        values[i * NUM_OF_CHANNELS + 0] = pixel.r;
+        values[i * NUM_OF_CHANNELS + 1] = pixel.g;
+        values[i * NUM_OF_CHANNELS + 2] = pixel.b;
+        i++;
+      });
+    
+      const outShape = [224, 224, NUM_OF_CHANNELS];
+      let img_tensor = tf.tensor3d(values, outShape, 'float32');
+      img_tensor = img_tensor.expandDims(0);
+    
+      const predictions = await model.predict(img_tensor).dataSync();
+    
+      for (let i = 0; i < predictions.length; i++) {
+        const probability = predictions[i];
+        console.log(`${probability}`);
+      }
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+}
+
 app.get("/", (req, res) => {
   res.json({ "Hello": "World!" });
 });
 
-app.post("/api/upload", upload.single("file"), (req, res) => {
+app.post("/api/upload", upload.single("file"), async (req, res) => {
   try {
-    console.log("file", req.file);
-    console.log("req", JSON.stringify(req.body));
+    console.log(req.file.path);
+    await getPredictions(req.file.path);
     res.status(200).json({
       message: "success!",
     });
